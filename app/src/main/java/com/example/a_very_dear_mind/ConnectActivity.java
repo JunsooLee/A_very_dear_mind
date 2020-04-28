5cbd279d9f143cef8f4c77c0476aad7ce617ed2a
JunsooLee <junsoolee65@naver.com>
2020-04-28 23:45:31 +0900

    Android

--- app/src/main/java/com/example/a_very_dear_mind/ConnectActivity.java
+++ app/src/main/java/com/example/a_very_dear_mind/ConnectActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class ConnectActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
 
-    EditText editText;
-    TextView textView;
+public class ConnectActivity extends AppCompatActivity {
 
     Handler handler = new Handler();
 
@@ -21,25 +27,38 @@ public class ConnectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
 
-        editText = findViewById(R.id.editText);
-        textView = findViewById(R.id.textView);
-
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                final String urlStr = editText.getText().toString();
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        request(urlStr);
-                    }
-                }).start();
+            public void run() {
+                request("URL");
+            }
+        }).start();
+    }
+
+    public void handlerView(final String data){
+        Log.d("Connect", data);
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
             }
         });
     }
+    public void request(String data){
+        try{
+            int portNumber = 5001;
+            Socket socket = new Socket("localhost", portNumber);
 
-    public void request(String urlStr){
+            DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
+            outStream.writeChars(data);
+            outStream.flush();
+
+            DataInputStream inStream = new DataInputStream(socket.getInputStream());
+            socket.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
--- app/src/main/java/com/example/a_very_dear_mind/ServerConnect.java
+++ app/src/main/java/com/example/a_very_dear_mind/ServerConnect.java
@@ -100,5 +100,4 @@ public class ServerConnect {
         return null;
 
     }
-
 }
